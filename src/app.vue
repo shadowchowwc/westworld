c33ab165057a3becaf901fac03f3168d418f1eb6
shadowchowwc <121296152+shadowchowwc@users.noreply.github.com>
2024-11-14 21:19:48 +0800

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,37 +1,47 @@
 <template>
   <RouterView/>
 </template>
-<script >
-import store from './store';
+
+<script>
+import robotScript from '@/auto/global/robot.screencapture.js';
+
 export default {
-  data () {
-    return {
-      store,
+  async created() {
+    try {
+      await auto.invoke('runRobotNow', [{ robot: robotScript }]);
+      console.log('调用全局截图权限');
+    } catch (error) {
+      console.error('调用截图权限时出错:', error);
     }
-  },
-  watch: {
-  },
-  created(){
-    auto.invoke('runRobotNow',[{robot:require('@/auto/global/robot.screencapture.js')}],()=>{
-      console.log('调用全局截图权限')
-    })
   }
 }
 </script>
 
 <style lang="less">
-#app{
+@fontSize: 20px;
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100vh;
   height: 100vh;
   overflow: hidden;
 }
-.container{
-  font-size: 20px;
-  p{
+
+.container {
+  font-size: @fontSize;
+
+  p {
     margin: 5px 0;
     text-indent: 2em;
   }
-  img{
-    width:80%;
+
+  img {
+    width: 80%;
   }
 }
 </style>
