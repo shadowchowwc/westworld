aca9318187bffcb81dc9981fb57756780993281d
shadowchowwc <121296152+shadowchowwc@users.noreply.github.com>
2024-11-15 01:38:49 +0800

    Update dailyQuests.vue

--- src/views/home/dailyQuests.vue
+++ src/views/home/dailyQuests.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="container">
-    <img style="width: 100%;" :src="require('@/assets/img/autoQuests.jpg')" />
-    <p class="bold-text">使用方法：</p>
-    <p>选择需要执行的任务，然后点击【启动机器人】按钮，最后点击悬浮窗口的【开始】按钮即可。</p>
-
-    <div class="task-options">
-      <van-radio-group v-model="groupChecked" direction="horizontal">
-        <div class="radio-row">
-          <van-radio name="normal2">精锐双本</van-radio>
-          <van-radio name="normal3">精锐三本</van-radio>
+    <van-nav-bar title="副本任务" left-text="返回" left-arrow @click-left="onClickLeft" />
+    <van-form @failed="onFailed" @submit="handleSubmit">
+      <img style="width: 100%;" :src="require('@/assets/img/autoQuests.jpg')" />
+      <p class="bold-text">使用方法：</p>
+      <p>选择需要执行的任务，然后点击【启动机器人】按钮，最后点击悬浮窗口的【开始】按钮即可。</p>
+      <div class="task-options">
+        <van-radio-group v-model="groupChecked" direction="horizontal">
+          <div class="radio-row">
+            <van-radio name="normal2">精锐双本</van-radio>
+            <van-radio name="normal3">精锐三本</van-radio>
+          </div>
+          <div class="radio-row">
+            <van-radio name="highNormal3">神威三本</van-radio>
+            <van-radio name="highNormal5">神威五本</van-radio>
+          </div>
+        </van-radio-group>
+        <div class="select-ghost">
+          <van-checkbox v-model="isInfiniteGhost">副本完成后无限抓鬼</van-checkbox>
         </div>
-        <div class="radio-row">
-          <van-radio name="highNormal3">神威三本</van-radio>
-          <van-radio name="highNormal5">神威五本</van-radio>
-        </div>
-      </van-radio-group>
-      
-      <div class="select-ghost">
-        <van-checkbox v-model="isInfiniteGhost">副本完成后无限抓鬼</van-checkbox>
       </div>
-    </div>
-
-    <div class="button-container">
-      <van-button 
-        :disabled="!groupChecked" 
-        round 
-        @click="startNow" 
-        type="primary" 
-        native-type="submit">
-        开始执行
-      </van-button>
-    </div>
+      <div class="button-container">
+        <van-button :disabled="!groupChecked" round @click="startNow" type="primary" native-type="submit">开始执行</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
